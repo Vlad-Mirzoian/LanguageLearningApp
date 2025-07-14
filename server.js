3ee15e328afaa801f676e2360d71fd1a562a87fb
Ukra1n1anUnd3rd0g <impersonalvlad23@gmail.com>
2025-07-14 20:36:51 +0300

    Created simple Express server with MongoDB connection

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/language-learning', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Language Learning API is running' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
